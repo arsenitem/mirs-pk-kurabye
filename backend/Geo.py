415aa49d53537820338e6d101f974ab899a174b7
Aranteus <51376801+Aranteus@users.noreply.github.com>
2020-09-13 12:53:26 +0500

    Рабочая версия модели

--- backend/Geo.py
+++ backend/Geo.py
@@ -4,27 +4,48 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import shapefile as shp
 from sklearn.utils.class_weight import compute_class_weight
+import os
 
 
-def main(X, y):
-    import_data()
-    # model = make_model(X, y)
-    # predict(model)
+def main():
+    X, y = import_data()
+    model = make_model(X, y)
+    #predict(model)
 
 
 def import_data():
-    shp_path = "./Слои/Поля_Полигональные2.shp"
-    sf = shp.Reader(shp_path)
-    print(len(sf.shapes()))
-    print(sf.records()[1])
+    from keras.preprocessing.image import load_img, img_to_array
+    # shp_path = "./Слои/Поля_Полигональные2.shp"
+    # sf = shp.Reader(shp_path)
+    # print(len(sf.shapes()))
+    # print(sf.records()[1])
+    files = os.listdir("cut_images_clean/")
+    files = [x for x in files if x.endswith(".jpg")]
+    x = []
+    y = []
+
+    for file in files:
+        if file.startswith("g"):
+            label = 1
+        else:
+            label = 0
+
+        img_path = os.path.join("cut_images_clean/", file)
+        img = load_img(img_path)
+        img = img_to_array(img) / 255
+        if img.size == 7686:
+            x.append(img)
+            y.append(label)
+
+    X = np.array(x)
+    y = np.array(y)
+    return X, y
+
+    
 
 
 def make_model(X, y):
-    import keras
     from keras import Sequential, layers
-
-    print(X.shape)
-    print(y.shape)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y)  # 20% тестовых данных
 
@@ -36,17 +57,13 @@ def make_model(X, y):
     cw = compute_class_weight("balanced", np.unique(y_train), y_train)
 
     model = Sequential([
-        layers.Conv2D(16, (3, 3), name="conv1", input_shape=(X.shape[1], X.shape[2], X.shape[3]), activation="relu",
+        layers.Conv2D(16, (3, 3), name="conv1", input_shape=(42, 61, 3), activation="relu",
                       padding="same"),
         layers.MaxPool2D((2, 2), name="pool1"),
         layers.Dropout(0.2),
         layers.Conv2D(32, (3, 3), name="conv2", padding="same"),
         layers.Activation("relu"),
         layers.MaxPool2D((2, 2), name="pool2"),
-        layers.Dropout(0.2),
-        layers.Conv2D(32, (3, 3), name="conv3", padding="same"),
-        layers.Activation("relu"),
-        layers.MaxPool2D((2, 2), name="pool3"),
         layers.Dropout(0.2),
         layers.Flatten(),
         layers.Dense(64, activation="relu"),
@@ -58,15 +75,14 @@ def make_model(X, y):
 
     model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["acc"])
 
-    model.summary()
-    model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test), class_weight=cw)
+    model.fit(X_train, y_train, epochs=15, validation_data=(X_test, y_test), class_weight=cw)
     return model
 
 
 def predict(model, image):
     import slidingwindow as sw
     subimages = []
-    shape = 56
+    shape = (42, 61)
     windows = sw.generate(image, sw.DimOrder.HeightWidthChannel, shape, 0.6)
     for i, window in enumerate(windows):
         _img = image[window.indices()]
@@ -108,6 +124,4 @@ def predict(model, image):
 
 if __name__ == "__main__":
     warnings.filterwarnings(action="ignore")
-    X = []
-    y = []
-    main(X, y)
+    main()
